de592c93fb2eaaf349460e9d5ee99e65e7ba5383
探姬 <41804496+ProbiusOfficial@users.noreply.github.com>
2023-12-08 11:32:40 +0000

    update

--- docs/javascripts/statis.js
+++ docs/javascripts/statis.js
@@ -1,8 +1,14 @@
 // 当文档加载完毕时执行
 document.addEventListener("DOMContentLoaded", function() {
+    // 获取当前页面的路径和查询字符串
+    var pathAndQuery = window.location.pathname + window.location.search;
+
+    // 构建新的URL
+    var newUrl = "https://ctf.probius.xyz" + pathAndQuery;
+
     // 显示一个警告消息给用户
-    // alert("由于CDN接入，站点已更改，正在重定向跳转到 ctf.probius.xyz.");
+    console("由于CDN接入，站点已更改，正在重定向跳转到 " + newUrl);
 
     // 重定向到新的网址
-    window.location.href = "https://ctf.probius.xyz/";
+    window.location.href = newUrl;
 });
