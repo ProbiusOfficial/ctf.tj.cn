a2cd3c3d11eae35aee343b8b5846b2f3969bd544
探姬 <41804496+ProbiusOfficial@users.noreply.github.com>
2023-12-08 19:23:09 +0800

    Update statis.js

--- docs/javascripts/statis.js
+++ docs/javascripts/statis.js
@@ -1,1 +1,8 @@
-document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"3G91BcC1MOsaapYy\",ck:\"3G91BcC1MOsaapYy\"});</script>")
+// 当文档加载完毕时执行
+document.addEventListener("DOMContentLoaded", function() {
+    // 显示一个警告消息给用户
+    // alert("由于CDN接入，站点已更改，正在重定向跳转到 ctf.probius.xyz.");
+
+    // 重定向到新的网址
+    window.location.href = "https://ctf.probius.xyz/";
+});
